72b439f0363231ef81dfc9ecd21785a79a695102
dusainbolt <dulh181199@gmail.com>
2020-09-30 20:18:52 +0700

    fix search

--- src/components/Admin/Header/index.jsx
+++ src/components/Admin/Header/index.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import LogoHeader from "../../../common/image/LOGO.png";
 import { SearchOutlined, ShoppingCartOutlined, UserOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import { MenuTop } from "../../../common/configLandingPage";
 import { onRedirect } from "../../../utils";
+import ModalCommon from "../../Modal";
+import { Input } from 'antd';
+
+const { Search } = Input;
+
 
 function CommonHeader({ toggleMenu, refHeader }) {
+
+  const [visibleSearch, setVisibleSearch] = useState(false);
 
   const renderMenuTop = () => {
     return MenuTop.map((item, index) => {
@@ -17,6 +24,28 @@ function CommonHeader({ toggleMenu, refHeader }) {
     });
   };
 
+  const openModalSearch = () => {
+    setVisibleSearch(true);
+  };
+
+  const onCloseModal = () => {
+    setVisibleSearch(false);
+  };
+
+  const renderContentSearch = () => {
+    return (
+      <div>
+        <Search placeholder="Nhập từ khóa tìm kiếm" onSearch={value => onRedirect("/blog")} enterButton />
+      </div>
+    );
+  };
+
+  const renderModalSearch = useMemo(()=>{
+      return (
+        <ModalCommon visible={visibleSearch} onCancel={onCloseModal}  content={renderContentSearch()} title="Tìm kiếm" />
+      )
+  },[visibleSearch]);
+
   return (
     <div className="header" id="headerWeb">
       <img className="header__logo" src={LogoHeader} onClick={() => onRedirect("/")} alt="logo" />
@@ -25,11 +54,12 @@ function CommonHeader({ toggleMenu, refHeader }) {
       </div>
       <div className="header__icon">
         <div className="header__icon-wrapper">
-          <SearchOutlined className="header__icon--item" />
+          <SearchOutlined onClick={() => openModalSearch()} className="header__icon--item" />
           <ShoppingCartOutlined onClick={() => onRedirect("/shoppingcart")} className="header__icon--item" />
           <UserOutlined onClick={() => onRedirect("/account")}  className="header__icon--item" />
         </div>
       </div>
+      {renderModalSearch}
     </div>
   );
 }
